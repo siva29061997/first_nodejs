0f440d2d0455343f5b8769b5c0ffec1be4394f29
siva29061997 <siva29061997@gmail.com>
2022-09-17 16:12:35 +0530

    crud cummit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cors = require("cors")
 const app = express()
-
+const mongodb = require("mongodb")
+const mongoClient = mongodb.MongoClient
+const URL = "mongodb://localhost:27017"
+const DB = "batch_37_wd_Tamil"
 let users = [];
 
 app.use(express.json());
@@ -9,69 +12,161 @@ app.use(cors({
     origin: "http://localhost:3001"
 }));
 
-app.get("/users", function (req, res) {
-    let qParms = req.query
-    console.log(qParms)
+app.get("/users",async function (req, res) {
 
-    let resUser = []
-
-    for (let index = parseInt(req.query.offset); index < parseInt(req.query.offset) + parseInt(req.query.limit); index++) {
-        if(users[index]){
-            resUser.push(users[index])
-        }
+    try {
+    // step 1:
+    const connection = await mongoClient.connect(URL)
+    // step 2:
+    const db = connection.db(DB);
+    // step 3:
+    // step 4:
+    let reUser = await db.collection("users").find().toArray();
+    // step 5:
+    await connection.close();
+    // error :
+    res.json(reUser);
+    } catch (error) {
+        res.status(500).json({
+            messege:"Somthig went wrong"
+        });
     }
+    // let qParms = req.query
+    // console.log(qParms)
 
-    res.json(resUser);
+    // let resUser = []
+
+    // for (let index = parseInt(req.query.offset); 
+    // index < parseInt(req.query.offset) + parseInt(req.query.limit); index++) {
+    //     if(users[index]){
+    //         resUser.push(users[index])
+    //     }
+    // }
 });
 
-app.post("/user", function (req, res) {
-    req.body.id = users.length + 1;
-    users.push(req.body)
-    res.json({ messege: "user created" });
+app.post("/user",async function (req, res) {
+    
+    try {
+        // step 1:
+    const connection = await mongoClient.connect(URL)
+    // step 2:
+    const db = connection.db(DB)
+    // step 3:
+    // step 4:
+    await db.collection("users").insertOne(req.body)
+    // step 5:
+    await connection.close()
+    // error :
+    res.status(200).json({messege:"Done"})
+    } catch (error) {
+        res.status(500).json({
+            messege:"Somthig went wrong"
+        })
+    }
+
+    // req.body.id = users.length + 1;
+    // users.push(req.body)
+    // res.json({ messege: "user created" });
 });
 
-app.get("/user/:id", function (req, res) {
-    let userId = req.params.id;
-    let user = users.find((item) => item.id == userId);
-    if (user) {
-        res.json(user)
-    } else {
-        res.json({ messege: "User not found" });
+app.get("/user/:id",async function (req, res) {
+
+    try {
+        // step 1:
+    const connection = await mongoClient.connect(URL)
+    // step 2:
+    const db = connection.db(DB)
+    // step 3:
+    // step 4:
+    let user = await db.collection("users").findOne({_id: mongodb.ObjectId(req.params.id)});
+    // step 5:
+    await connection.close()
+    // error :
+    res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({
+            messege:"Somthig went wrong"
+        })
     }
+
+    // let userId = req.params.id;
+    // let user = users.find((item) => item.id == userId);
+    // if (user) {
+    //     res.json(user)
+    // } else {
+    //     res.json({ messege: "User not found" });
+    // }
 })
 
-app.put("/user/:id", function (req, res) {
-    let userId = req.params.id;
-    let userIndex = users.findIndex((item) => item.id == userId);
+app.put("/user/:id",async function (req, res) {
 
-    if (userIndex != -1) {
-        Object.keys(req.body).forEach((item) => {
-            users[userIndex][item] = req.body[item]
-        });
-        res.json({
-            messege: "Done"
-        });
-    } else {
-        res.json({
-            messege: "User not found"
-        });
+    try {
+        // step 1:
+    const connection = await mongoClient.connect(URL)
+    // step 2:
+    const db = connection.db(DB)
+    // step 3:
+    // step 4:
+    let user = await db.collection("users").findOneAndUpdate({_id: mongodb.ObjectId(req.params.id)},{$set:req.body})
+    // step 5:
+    await connection.close()
+    // error :
+    res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({
+            messege:"Somthig went wrong"
+        })
     }
+    // let userId = req.params.id;
+    // let userIndex = users.findIndex((item) => item.id == userId);
+
+    // if (userIndex != -1) {
+    //     Object.keys(req.body).forEach((item) => {
+    //         users[userIndex][item] = req.body[item]
+    //     });
+    //     res.json({
+    //         messege: "Done"
+    //     });
+    // } else {
+    //     res.json({
+    //         messege: "User not found"
+    //     });
+    // }
 });
 
-app.delete("/user/:id", function (req, res) {
-    let userId = req.params.id;
-    let userIndex = users.findIndex((item) => item.id == userId);
+app.delete("/user/:id",async function (req, res) {
 
-    if (userIndex != -1) {
-        users.splice(userIndex, 1);
-        res.json({
-            messege: "User deleted"
-        });
-    } else {
-        res.json({
-            messege: "User not found"
-        });
+    try {
+        // step 1:
+    const connection = await mongoClient.connect(URL)
+    // step 2:
+    const db = connection.db(DB)
+    // step 3:
+    // step 4:
+    let user = await db.collection("users").findOneAndDelete({_id: mongodb.ObjectId(req.params.id)});
+    // step 5:
+    await connection.close()
+    // error :
+    res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json({
+            messege:"Somthig went wrong"
+        })
     }
+
+    // let userId = req.params.id;
+    // let userIndex = users.findIndex((item) => item.id == userId);
+
+    // if (userIndex != -1) {
+    //     users.splice(userIndex, 1);
+    //     res.json({
+    //         messege: "User deleted"
+    //     });
+    // } else {
+    //     res.json({
+    //         messege: "User not found"
+    //     });
+    // }
 });
 
 app.listen(process.env.PORT || 3000);
